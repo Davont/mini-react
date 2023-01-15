625e6a662b1a2ae3411caac8426e1f0c12a0fa39
davont <zhang980511@outlook.com>
2023-01-16 04:33:01 +0800

    feat: react 调试方法1更新

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,54 @@
+import { getPackageJSON, resolvePkgPath, getBaseRollupPlugin } from './utils';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+
+const { name, module } = getPackageJSON('react');
+// react 包路径
+
+const pkgPath = resolvePkgPath(name);
+
+// react 产物路径
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+	// react
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			name: 'react',
+			file: `${pkgDistPath}/index.js`,
+			format: 'umd'
+		},
+		plugins: [
+			...getBaseRollupPlugin(),
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, description, version }) => ({
+					name,
+					description,
+					version,
+					main: 'index.js'
+				})
+			})
+		]
+	},
+	// jsx-runtime
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			// jsx-runtime
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime.js',
+				format: 'umd'
+			},
+			// jsx-dev-runtime
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime.js',
+				format: 'umd'
+			}
+		],
+		plugins: getBaseRollupPlugin()
+	}
+];
