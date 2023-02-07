84e20557304e1125ee0ea0653e1007346f78a50e
davont <zhang980511@outlook.com>
2023-02-07 23:42:17 +0800

    feat: 实现mount流程的beginWork

--- packages/react-reconciler/src/beginWork.ts
+++ packages/react-reconciler/src/beginWork.ts
@@ -1,5 +1,56 @@
 // 递归中的 递阶段
 
-export const beginWork = (fiber) => {
+import { ReactElementType } from 'shared/ReactTypes';
+import { FiberNode } from './fiber';
+import { UpdateQueue, processUpdateQueue } from './updateQuene';
+import { HostComponent, HostRoot, HostText } from './workTags';
+import { mountChildFibers, reconcileChildFibers } from './childFibers';
+
+export const beginWork = (wip: FiberNode) => {
 	// 比较，最终会返回 子 fiberNode
+	switch (wip.tag) {
+		case HostRoot:
+			return updateHostRoot(wip);
+		case HostComponent:
+			return updateHostComponent(wip);
+		case HostText:
+			return null; // 递归的“递”阶段结束，下面一步是“归”阶段
+		default:
+			if (__DEV__) {
+				console.warn('beginWork 未实现的类型');
+			}
+			break;
+	}
 };
+function updateHostRoot(wip: FiberNode) {
+	const baseState = wip.memoizedState;
+	const updateQueue = wip.updateQueue as UpdateQueue<Element>;
+	const pending = updateQueue.shared.pending;
+	updateQueue.shared.pending = null;
+	const { memoizedState } = processUpdateQueue(baseState, pending);
+	wip.memoizedState = memoizedState;
+
+	const nextChildren = wip.memoizedState;
+	reconcileChildren(wip, nextChildren);
+	return wip.child;
+}
+
+// updateHostComponent 不像 updateHostRoot，它是不会触发更新的，
+// 所以没有更新的过程，只有创建子fiberNode
+function updateHostComponent(wip: FiberNode) {
+	const nextProps = wip.pendingProps;
+	const nextChildren = nextProps.chilren;
+	reconcileChildren(wip, nextChildren);
+	return wip.child;
+}
+
+function reconcileChildren(wip: FiberNode, children?: ReactElementType) {
+	const current = wip.alternate;
+
+	if (current !== null) {
+		// update
+		wip.child = reconcileChildFibers(wip, current?.child, children);
+	} else {
+		wip.child = mountChildFibers(wip, null, children);
+	}
+}
